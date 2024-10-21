8ee66a1feffb99e91cb776c9bee7c7aff38dd2cc
erwind21 <erwind605@gmail.com>
2024-10-21 19:52:28 +0700

    Codelab1_3

---
+++ src/DebugExample.java
@@ -0,0 +1,8 @@
+public class DebugExample {
+    public static void main(String[] args) {
+        int x  = 5;
+        int y  = 10;
+        int sum = x +y ;
+        System.out.println("Sum:" + sum );
+    }
+}
